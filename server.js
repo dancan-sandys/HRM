890d0b47243e34bf56aedf8fd85b6c56aaf59bc5
dancan-sandys <dancan.oruko96@gmail.com>
2021-01-26 04:07:52 +0300

    push to heroku

--- server.js
+++ server.js
@@ -1,27 +1,16 @@
+//Install express server
 const express = require('express');
-const cors = require('cors')
-const http = require('http');
 const path = require('path');
 
-
-const port = process.env.PORT || 3001;
 const app = express();
 
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/<name-of-app>'));
 
+app.get('/*', function (req, res) {
 
-
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Orgin,X-Requested-WIdth, Content-Type, Accept");
-    next();
+    res.sendFile(path.join(__dirname + '/dist/<name-of-app>/index.html'));
 });
 
-
-
-app.use(express.static(__dirname + '/dist/hrmfrontend'));
-
-app.get('/*', (req, res) => res.sendFile(path.join(__dirname)));
-
-const server = http.createServer(app);
-
-server.listen(port, () => console.log(`App running on: http://localhost:${port}`));
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
