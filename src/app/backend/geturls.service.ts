803f5bf184c71f92f236f3035bb5339bbfd9c908
dancan-sandys <dancan.oruko96@gmail.com>
2021-01-26 04:11:27 +0300

    replace the local backend urls with deployed urls

--- src/app/backend/geturls.service.ts
+++ src/app/backend/geturls.service.ts
@@ -17,22 +17,22 @@ export class GeturlsService {
   httpHeaders:HttpHeaders =new HttpHeaders({'Content-Type':'application/json'})
 
   get(uri: any) {
-    return this.http.get(`http://localhost:8000/${uri}`,
+    return this.http.get(`https://sandyshr.herokuapp.com/${uri}`,
     {headers: this.httpHeaders})
   }
 
   post(uri: any, content: any) {
-    return this.http.post(`http://localhost:8000/${uri}`, content,
+    return this.http.post(`https://sandyshr.herokuapp.com/${uri}`, content,
       { headers: this.httpHeaders })
   }
 
   delete(uri: any, content: any) {
-    return this.http.post(`http://localhost:8000/${uri}`, content,
+    return this.http.post(`https://sandyshr.herokuapp.com/${uri}`, content,
       { headers: this.httpHeaders })
   }
 
   put(uri: any, content: any) {
-    return this.http.put(`http://localhost:8000/${uri}`, content,
+    return this.http.put(`https://sandyshr.herokuapp.com/${uri}`, content,
       { headers: this.httpHeaders })
   }
 
